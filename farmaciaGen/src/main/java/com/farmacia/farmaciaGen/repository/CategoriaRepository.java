44d9d990d3bc764749d138e44e109d9ad74e5222
Silvia Santana <oliversilvia20@gmail.com>
2022-10-10 18:54:41 -0300

    Back-End Farm

---
+++ farmaciaGen/src/main/java/com/farmacia/farmaciaGen/repository/CategoriaRepository.java
@@ -0,0 +1,18 @@
+package com.farmacia.farmaciaGen.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.farmacia.farmaciaGen.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+	
+
+
+}
